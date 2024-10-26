f0a061106a83cb0563bcdc8ff48b0bebdbee3853
German Castellanos <germancastellanose@gmail.com>
2024-10-26 05:03:00 -0600

    feat:

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +13,7 @@ export default defineConfig({
       "/api": {
         target: process.env.VITE_API_URL || "http://localhost:5000", // Usa VITE_API_URL o el valor por defecto si no está definido
         changeOrigin: true,
-        secure: false, // Puedes cambiar esto a true si tu API requiere HTTPS y tiene certificado válido
+        secure: false, // Cambiar a true si necesitas que la conexión sea segura
       },
     },
   },
