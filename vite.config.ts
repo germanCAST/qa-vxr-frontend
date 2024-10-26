6ec8686d5d5e1cb0577cc1aee7299270899066c4
German Castellanos <germancastellanose@gmail.com>
2024-10-26 05:10:39 -0600

    feat: :fire:

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:5000", // Usa VITE_API_URL o el valor por defecto si no está definido
+        target: import.meta.env.VITE_SUPABASE_URL || "http://localhost:5000", // Usa VITE_API_URL o el valor por defecto si no está definido
         changeOrigin: true,
         secure: false, // Cambiar a true si necesitas que la conexión sea segura
       },
