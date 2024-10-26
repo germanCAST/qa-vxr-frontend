b97b466cc4b9a1725ce27d204f8f03f100303855
German Castellanos <germancastellanose@gmail.com>
2024-10-26 04:35:06 -0600

    feat: :fire:

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      // Solo usar el proxy durante el desarrollo
+      proxy:
+        mode === "development"
+          ? {
+              "/api": env.VITE_API_URL,
+            }
+          : undefined,
     },
-  },
+  };
 });
